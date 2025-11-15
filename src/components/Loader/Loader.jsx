5fd71055e7e53ceab81a9a983138b19adfca881f
AsHrAf KaMaL <ashrafk0205@gmail.com>
2025-11-15 13:11:21 +0530

    new changes in loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,12 +2,22 @@ import "./loader.css"
 
 function Loader() {
   return (
-    <div class="loader mt-10 mx-auto">
-      <div class="box1"></div>
-      <div class="box2"></div>
-      <div class="box3"></div>
-    </div>
-  );
+    <div class="spinner loader mt-20 mb-10 mx-auto">
+      <div></div>   
+      <div></div>    
+      <div></div>    
+      <div></div>    
+      <div></div>    
+      <div></div>    
+      <div></div>    
+      <div></div>    
+      <div></div>    
+      <div></div>    
+    </div>)
 }
 
 export default Loader;
+
+// <!-- From Uiverse.io by PriyanshuGupta28 --> 
+// <div class="spinner">
+  
